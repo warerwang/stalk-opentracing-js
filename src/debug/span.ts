8ab78438d7c01d7e2a35c6219722e7afcc83cdc1
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-05-24 17:31:36 +0300

    Debug tracer

---
+++ src/debug/span.ts
@@ -0,0 +1,24 @@
+import * as opentracing from '../opentracing/index';
+import DebugTracer from './tracer';
+import BaseSpan from '../base/span';
+
+
+export default class DebugSpan extends BaseSpan {
+    /**
+     * Override just for returning tracer's type
+     */
+    tracer(): DebugTracer {
+        return super.tracer() as DebugTracer;
+    }
+
+
+    /**
+     * Override BaseSpan's internal `_log` method to interfere logs.
+     * Not calling `super._log()` method to not store logs, efficiency-stuff
+     */
+    protected _log(keyValuePairs: { [key: string]: any }, timestamp?: number) {
+        const tracer = this.tracer();
+        const component = this.getTag(opentracing.Tags.COMPONENT);
+        tracer.forward(component, keyValuePairs as any);
+    }
+}
