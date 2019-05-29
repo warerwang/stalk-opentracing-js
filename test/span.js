897f290ace97c4ef43db9a961b06d5dc65b3c6c1
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-05-29 12:46:00 +0300

    StalkSpan behaviour tests

---
+++ test/span.js
@@ -0,0 +1,105 @@
+const expect = require('chai').expect;
+const stalk = require('../');
+
+
+describe('StalkSpan behaviour tests', function() {
+    let tracer = new stalk.StalkTracer();
+    let spanContext = new stalk.StalkSpanContext('xxxTraceIdxxx', 'xxxSpanIdxxx');
+    let span;
+
+
+    beforeEach(function() {
+        span = new stalk.StalkSpan(tracer, spanContext);
+    });
+
+
+    it('should return tracer instance when `.tracer()` called', function() {
+        expect(span.tracer()).to.equal(tracer);
+    });
+
+
+    it('should return context instance when `.context()` called', function() {
+        expect(span.context()).to.equal(spanContext);
+    });
+
+    it('should set current time when `.start()` called without any parameters', function() {
+        const now = Date.now();
+        span.start();
+        // Start time can be equal to now, or above. We're extracting 1 ms to cover both.
+        expect(toJSON(span).startTime).to.be.above(now - 1);
+    });
+
+    it('should set specific time when `.start(specificTime)` called', function() {
+        const specificTime = Date.now() - 1000;
+        span.start(specificTime);
+        expect(toJSON(span).startTime).to.equal(specificTime);
+    });
+
+    it('should add reference when `.addReference(ref)` called', function() {
+        let parentContext = new stalk.StalkSpanContext('xxxTraceIdxxx', 'xxxParentSpanIdxxx');
+        const ref = new stalk.Reference(stalk.REFERENCE_CHILD_OF, parentContext);
+        span.addReference(ref);
+        const resultRefs = toJSON(span).references;
+        expect(resultRefs).to.have.lengthOf(1);
+        expect(resultRefs[0].type).to.equal(stalk.REFERENCE_CHILD_OF);
+        expect(resultRefs[0].referencedContext.traceId).to.equal('xxxTraceIdxxx');
+        expect(resultRefs[0].referencedContext.spanId).to.equal('xxxParentSpanIdxxx');
+    });
+
+    it('should set operation name when `.setOperationName(name)` called', function() {
+        span.setOperationName('Working hard, you know');
+        expect(toJSON(span).operationName).to.equal('Working hard, you know');
+    });
+
+    it('should add tags over the current ones when `.addTags(tags)` called', function() {
+        span.setTag('tag1', 'value1');
+        span.addTags({ tag2: 'value2', tag3: 'value3' });
+        span.addTags({ tag3: 'value3-override' });
+        const resultTags = toJSON(span).tags;
+        expect(Object.keys(resultTags)).to.have.lengthOf(3);
+        expect(resultTags).to.have.property('tag1', 'value1');
+        expect(resultTags).to.have.property('tag2', 'value2');
+        expect(resultTags).to.have.property('tag3', 'value3-override');
+    });
+
+    it('should get specific tag when `.getTag(tag)` called', function() {
+        span.addTags({ tag1: 'value1', tag2: 'value2' });
+        expect(span.getTag('tag1')).to.equal('value1');
+        expect(span.getTag('tag2')).to.equal('value2');
+        expect(span.getTag('non-existing-tag')).to.equal(undefined);
+    });
+
+    it('should set current time when `.finish()` called without any parameters', function() {
+        const now = Date.now();
+        span.finish();
+        // Finish time can be equal to now, or above. We're extracting 1 ms to cover both.
+        expect(toJSON(span).finishTime).to.be.above(now - 1);
+    });
+
+    it('should set specific time when `.finish(specificTime)` called', function() {
+        const specificTime = Date.now() - 1000;
+        span.finish(specificTime);
+        expect(toJSON(span).finishTime).to.equal(specificTime);
+    });
+
+    it('should put logs with `.logger` API', function() {
+        // TODO: Payload tests
+        span.logger.silly('silly log message');
+        span.logger.debug('debug log message');
+        span.logger.info('info log message');
+        span.logger.warn('warning log message');
+        span.logger.error('error log message');
+        const resultLogs = toJSON(span).logs;
+        expect(resultLogs).to.have.lengthOf(5);
+        expect(resultLogs[0].fields.message).to.equal('silly log message');
+        expect(resultLogs[1].fields.message).to.equal('debug log message');
+        expect(resultLogs[2].fields.message).to.equal('info log message');
+        expect(resultLogs[3].fields.message).to.equal('warning log message');
+        expect(resultLogs[4].fields.message).to.equal('error log message');
+    });
+});
+
+
+function toJSON(object) {
+    return JSON.parse(JSON.stringify(object));
+}
